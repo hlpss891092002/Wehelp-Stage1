8ecbc594696df24621a8ed6308f17cb7d5310aa9
hlpss891092002 <146625541+hlpss891092002@users.noreply.github.com>
2024-04-16 19:57:23 +0800

    task1 & task2  over

---
+++ week3/task1.py
@@ -0,0 +1,59 @@
+import urllib.request as request
+import json
+import csv
+src1="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+src2="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2"
+ASSIGNMENT_LIST=[]
+MRT_list=[]
+MRT_and_spot={}
+def get_assignment1_data():
+  with request.urlopen(src1) as response:
+    data=json.load(response)
+  # data=response.read().decode("utf-8")
+  assignment1=data["data"]["results"]
+  for list1 in assignment1:
+    jpg_index=list1["filelist"].lower().index("jpg")
+    list1["photo"]=list1["filelist"][0:jpg_index+3]
+    ASSIGNMENT_LIST.append(list1)
+def render_assignment2_data_to_assignment_list():
+  with request.urlopen(src2) as response:
+    data=json.load(response)
+    # data=response.read().decode("utf-8")
+  assignment2=data["data"]
+  assignment2List=[]
+  for list2 in assignment2:
+    list2["address"]=list2["address"].split()
+    list2["district"]=list2["address"][1][0:3]
+    assignment2List.append(list2)
+  for list1 in ASSIGNMENT_LIST:
+    for list2 in assignment2List:
+      if (list1["SERIAL_NO"] == list2["SERIAL_NO"] ):
+        list1.update(list2)
+def distinct_from_station():
+  MRT_set=set()
+  for dict in ASSIGNMENT_LIST:
+    MRT_set.add(dict["MRT"])
+  for item in MRT_set:
+    MRT_list.append(item)
+  for item in MRT_list:
+    MRT_and_spot[item]=[]
+  for item in ASSIGNMENT_LIST:
+    for title in MRT_list:
+      if item["MRT"]==title:
+        MRT_and_spot[title].append(item["stitle"])   
+  for item in MRT_and_spot:
+    MRT_and_spot[item]=",".join(MRT_and_spot[item])
+    print
+get_assignment1_data()
+render_assignment2_data_to_assignment_list()
+distinct_from_station()
+
+with open("spot.csv", mode="w", encoding="utf-8", newline="") as csvfile:
+  writer = csv.writer(csvfile)
+  for row in ASSIGNMENT_LIST:
+    writer.writerow([row["stitle"], row["district"], row["longitude"], row["latitude"], row["photo"]])
+with open("mrt.csv", mode="w", encoding="utf-8", newline="") as csvfile:
+  writer = csv.writer(csvfile)
+  for key in MRT_and_spot:
+    # writer.writerow([key, MRT_and_spot[key]])
+    csvfile.write(key+","+MRT_and_spot[key]+"\n")
---
+++ week3/task2.py
@@ -0,0 +1,73 @@
+import urllib.request as req
+import csv
+all_data_list=[]
+def get_data(url):
+  request=req.Request(url, headers={
+    "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+    "Cookie":"over18=1"
+  })
+  with req.urlopen(request) as response:
+    data=response.read().decode("utf-8")
+  import bs4
+  root=bs4.BeautifulSoup(data, "html.parser")
+  article_title=[]
+  article_likes=[]
+  article_time=[]
+  all_list=[]
+  def get_titles():
+    titles=root.find_all("div", class_="title")
+    mark=root.find_all("")
+    for title in titles:
+      if title.a != None :
+        title_string=(title.a.string)
+        article_title.append(title_string)
+  def get_likes():
+    nrecs=root.find_all("div", class_="nrec")
+    for nrec in nrecs:
+      if nrec.find_next_sibling("div").a==None :
+        continue
+      elif nrec.span == None:
+        article_likes.append("0")
+      else:
+        like=nrec.span.string
+        article_likes.append(like)
+  def get_time():
+     for title in article_title:
+      title_content="https://www.ptt.cc"+root.find("a", string=title)["href"]
+      request=req.Request(title_content, headers={
+        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+        "Cookie":"over18=1"
+      })
+      with req.urlopen(request) as response:
+        data=response.read().decode("utf-8")
+      root1=bs4.BeautifulSoup(data, "html.parser")
+      time_list = root1.find_all("span", class_="article-meta-value")
+      if len(time_list)==0:
+        article_time.append("")
+      else:
+        article_time.append(time_list[3].string)
+  def render_all_data():
+    i=0
+    while i<len(article_likes):
+      list=[]
+      list.append(article_title[i])
+      list.append(article_likes[i])
+      list.append(article_time[i])
+      str0=",".join(list)
+      all_data_list.append(str0)
+      i+=1
+  get_titles()
+  get_likes()
+  get_time()
+  render_all_data()
+    
+  previousPage=root.find("a", string="‹ 上頁")
+  return previousPage["href"]
+pageURL="https://www.ptt.cc/bbs/Lottery/index.html"
+count=0
+while count<3:
+  pageURL="https://www.ptt.cc"+get_data(pageURL)
+  count+=1
+with open("article.csv", mode="w", encoding="utf-8",newline="") as csvfile:
+  for article in all_data_list:
+    csvfile.write(article+"\n")
