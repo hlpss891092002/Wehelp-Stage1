9f6b93184f25b825a9cb27331d6f4604c408a819
hlpss891092002 <146625541+hlpss891092002@users.noreply.github.com>
2024-04-18 21:21:28 +0800

    no message

--- week3/js-page/index.js
+++ week3/js-page/index.js
@@ -1,69 +1,137 @@
 let hamburgerIcon = document.querySelector(".hamburger-icon");
 let hamburgerList = document.querySelector(".hamburger-list");
 let closeIcon = document.querySelector(".close-icon");
-let smBOx= document.querySelectorAll(".sm-box")
+let boxContainer = document.querySelector(".box-container")
+let smallBoxContainer = document.querySelector(".small-box-container");
+let bigBoxContainer = document.querySelector(".big-box-container");
+let windowWidth1200 = window.matchMedia("(max-width: 1200px)");
+let moreButton = document.querySelector(".more-info") ;
+let rawData=[];
+let count = 0;
+let bigBoxStart = 3;
+let loadAmount = 10;
+// 開啟/關閉側邊攔
 hamburgerIcon.addEventListener('click', function(event) {
-  hamburgerList.style.display = 'flex';
-})
+  hamburgerList.className="hamburger-list-display";
+});
 closeIcon.addEventListener('click', function(event){
-  hamburgerList.style.display = 'none';
-})
-fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1")
-.then(function(response){
-    return response.json();})
-.then((function(data){
-  results = data.data.results;//台北市景點文字資料
-  let divList=[];
-  let sm1 = document.querySelectorAll(".sm-1");
-  let sm2 = document.querySelectorAll(".sm-2");
-  let sm3 = document.querySelectorAll(".sm-3");
-  let bg1 = document.querySelectorAll(".bg-1");
-  let bg2 = document.querySelectorAll(".bg-2");
-  let bg3 = document.querySelectorAll(".bg-3");
-  let bg4 = document.querySelectorAll(".bg-4");
-  let bg5 = document.querySelectorAll(".bg-5");
-  let bg6 = document.querySelectorAll(".bg-6");
-  let bg7 = document.querySelectorAll(".bg-7");
-  let bg8 = document.querySelectorAll(".bg-8");
-  let bg9 = document.querySelectorAll(".bg-9");
-  let bg10 = document.querySelectorAll(".bg-10");
-  divList.push(sm1,sm2,sm3,bg1,bg2,bg3,bg4,bg5,bg6,bg7,bg8,bg9,bg10);
-  // 
-  for(let i=0; i < 3; i++){
-    renderSMImg(divList[i],i);
-    renderSMPromotion(divList[i],i)
-  };
-  function renderSMImg(divs, num){
-    let img = results[num].filelist.toLowerCase().slice(0,results[num].filelist.toLowerCase().indexOf("jpg")+3);
-    for (div of divs){
-      let image = div.querySelector(".sm-img");
-      image.src = img;
-    };
-  };
-  function renderSMPromotion(divs, num){
-    let title = results[num].stitle;
-    for (div of divs){
-      let promotion = div.querySelector("p");
-      promotion.textContent = title;
-    };
-  };
-  // render大方塊資料
-  for(let i = 3; i < 14;i++){
-    renderBGBackgroundImgAndTitle(divList[i],i);
-  };
-  function renderBGBackgroundImgAndTitle(divs,num){
-    let img = results[num].filelist.toLowerCase().slice(0,results[num].filelist.toLowerCase().indexOf("jpg")+3);
-    let title = results[num].stitle;
-      for( div of divs){
-        div.style.backgroundImage= `url("${img}")`;
-        div.style.bcakgroundSize="100%";
-        let p = div.querySelector("p");
-        p.textContent = `${title}`;
-        p.style.display = "-webkit-box";
-        p.style.webkitBoxOrient = "vertical";
-        p.style.webkitLineClamp = "1";
-        p.style.wordWrap ="normal";
+  hamburgerList.className="hamburger-list";
+});
 
-      };
+
+// 連線資料
+fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1")
+.then((response)=>{
+  return response.json();
+}).then((data)=>{
+  let results = data.data.results;
+  function renderSmallBoxData (){
+    for( let i = 0; i < 3; i++) {
+    let title = results[i].stitle;
+    let imgSrc = results[i].filelist.toLowerCase();
+    let firstImg = imgSrc.slice(0,imgSrc.indexOf("jpg")+3)
+    let smallBox = document.createElement("div");//創造小箱子
+    smallBox.className = `small-box sb-${i}`;
+    let smallImg = document.createElement("img");//創造圖片
+    smallImg.className="small-img";
+    smallImg.src=`${firstImg}`;
+    smallImg.alt = "small-image";
+    let smallTitle = document.createElement('p');//創造文字框
+    smallTitle.className="small-title";
+    smallTitle.textContent=`${title}`;
+    smallBox.appendChild(smallImg);//推smallBox
+    smallBox.appendChild(smallTitle); 
+    smallBoxContainer.appendChild(smallBox);//嘉進container
     };
-}));
+  };
+  
+  function renderBigBoxData(start, amount){
+    for( let i = start; i < start + amount; i++) {
+    let title = results[i].stitle;
+    let imgSrc = results[i].filelist.toLowerCase();
+    let firstImg = imgSrc.slice(0,imgSrc.indexOf("jpg")+3)
+    let bigBox = document.createElement("div");//創造小箱子
+    bigBox.className = `big-box bg-${count}`;
+    bigBox.style.backgroundImage=`url(${firstImg})`
+    let bigTitle = document.createElement('p');//創造文字框
+    bigTitle.textContent=`${title}`;
+    bigTitle.className="big-title";
+    let star = document.createElement('img');
+    star.src="./icon/star.png";
+    star.className="star";
+    bigBox.appendChild(star);
+    bigBox.appendChild(bigTitle); //推bigBox
+    bigBoxContainer.appendChild(bigBox);//嘉進container
+    count+=1;
+    };
+    bigBoxStart = start + amount;
+    changeLastBoxSize();
+  };
+  function clearBigBoxGridCss(){
+    AllBigBox = document.querySelectorAll(".big-box");
+    for( box of AllBigBox ){
+      box.style.gridColumnEnd="auto";
+      box.style.gridColumnStart="auto";
+    };
+  };
+  function changeLastBoxSize(){ 
+    if(window.innerWidth <= 1200 && count % 4 ===2 ){
+      let last1 = document.querySelector(`.bg-${count-2}`);
+      let last2 = document.querySelector(`.bg-${count-1}`);
+      last1.style.gridColumnStart = "1";
+      last1.style.gridColumnEnd = "3";
+      last2.style.gridColumnStart = "3";
+      last2.style.gridColumnEnd = "5";
+    }else if(window.innerWidth <= 1200 && count % 4 ===3 ){
+      let last1 = document.querySelector(`.bg-${count-1}`);
+      last1.style.gridColumnStart = "3";
+      last1.style.gridColumnEnd = "5";
+    }
+  }
+  function clearLastBoxGrid(){ 
+    if(window.innerWidth >= 1200 && count % 4 ===2 ){
+      let last1 = document.querySelector(`.bg-${count-2}`);
+      let last2 = document.querySelector(`.bg-${count-1}`);
+      last1.style.gridColumnStart = "auto";
+      last1.style.gridColumnEnd = "auto";
+      last2.style.gridColumnStart = "auto";
+      last2.style.gridColumnEnd = "auto";
+    }else if(window.innerWidth >= 1200 && count % 4 ===3 ){
+      let last1 = document.querySelector(`.bg-${count-1}`);
+      last1.style.gridColumnStart = "auto";
+      last1.style.gridColumnEnd = "auto";
+    }
+  }
+
+  renderSmallBoxData();
+  renderBigBoxData(bigBoxStart,loadAmount);
+  
+
+  // more 監聽器+計算剩餘數據數
+  moreButton.addEventListener("click",(event)=>{
+    clearBigBoxGridCss();
+    renderBigBoxData(bigBoxStart,loadAmount);
+    let remain = (results.length)-bigBoxStart;
+    if(remain < loadAmount && remain > 0){
+      loadAmount = remain;
+    }else if (remain === 0){
+      moreButton.style.display="none";
+      let loadOver = document.createElement("div");
+      loadOver.className="load-over";
+      let loadOverContent = document.createElement("p");
+      loadOverContent.className="load-over-content";
+      loadOverContent.textContent="All data are loaded";
+      loadOver.appendChild(loadOverContent);
+      boxContainer.appendChild(loadOver);
+    };
+
+}); 
+  window.addEventListener("resize", (event) => {
+    if( window.innerWidth <= 1200) {
+      changeLastBoxSize()
+    }else{
+      clearLastBoxGrid()
+    }
+  })
+})
+
